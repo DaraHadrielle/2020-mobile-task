617281506b249671b440fc7d7488ed41ea286d2a
DaraHadrielle <darahadrielle@gmail.com>
2020-03-12 08:28:19 -0300

    my first react web app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meu primeiro app mobile!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
